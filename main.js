bc627a168a5b33c42639174834836df3cee57084
Preston Fassbender <fassbenderp0551@gmail.com>
2025-01-07 14:37:41 -0600

    initial commit, transfire files to new repo

---
+++ main.js
@@ -0,0 +1,232 @@
+// HELLO WORLD
+// console.log("hello from main.js")
+
+
+// VARIABLES
+// let age = 28
+// console.log(age)
+
+// const salary = 80000
+// trying to reassign a const variable results in an error
+// salary = 99999
+// console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+
+// DATA TYPES
+// const name = "Preston"
+// const language = 'JavaScript'
+// const sport = `soccer`
+
+// const total = 0
+// const PI = 3.14
+
+// const isPrimaryNumber = true
+// const isNewUser = false
+
+// let result
+// console.log(result)
+// const res = undefined
+
+// let data = null
+
+// const person = {
+//     firstName: 'Preston',
+//     lastName: 'Fassbender',
+//     age: 28
+// }
+// console.log(person.firstName)
+
+// const animals = ['monkey', 'snake', 'frog', 'cat', 'dog']
+// console.log(animals[0])
+// console.log(animals[2])
+
+// let a = 10
+// a = 'awesome'
+// a = true
+// console.log(a)
+
+
+// OPERATORS
+// let x = 10
+// let y = 5
+
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+
+// console.log(++x)
+// console.log(--y)
+
+// console.log(x == y)
+// console.log(x != y)
+// console.log(x === y)
+// console.log(x !== y)
+// console.log(x > y)
+// console.log(x >= y)
+// console.log(x < y)
+// console.log(x <= y)
+// console.log(x == y)
+
+// const isValidNumber = x > 8 && 8 > y
+// console.log(isValidNumber)
+
+// const isValidNumber2 = x > 20 || 8 > y
+// console.log(isValidNumber2)
+
+// const isValid = true
+// console.log(!isValid)
+
+// console.log('Preston ' + 'Fassbender')
+
+// const isEven = 10 % 2 === 0 ? true : false
+// console.log(isEven)
+// const isEven2 = 10 % 2 === 0 ? 'number is even' : 'number is odd'
+// console.log(isEven2)
+
+
+// TYPE CONVERSION
+// console.log(2 + '3')
+// console.log(typeof (2 + '3'))
+
+// console.log(true + '3')
+// console.log(typeof (true + '3'))
+
+// console.log('4' - '2')
+// console.log(typeof ('4' - '2'))
+
+// console.log('cool' - 'stuff')
+
+// console.log('5' - true)
+// console.log('5' - false)
+// console.log('5' - null)
+
+// console.log(5 + undefined)
+
+// console.log(Number('5'))
+// console.log(Number(false))
+// console.log(Number(''))
+
+// console.log(parseInt('5'))
+// console.log(parseFloat('3.14'))
+
+// console.log(String(500))
+// console.log(String(true))
+// console.log(String(null))
+// console.log(String(undefined))
+
+// console.log((500).toString())
+
+// console.log(Boolean(10))
+
+
+// EQUALITY
+// let var1
+// let var2
+
+// var1 = 'test'
+// var2 = 'test'
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// var1 = 10
+// var2 = '10'
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// var1 = 0
+// var2 = ''
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// var1 = false
+// var2 = ''
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// var1 = null
+// var2 = undefined
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+
+// CONDITIONAL STATEMENTS
+// let num = -5
+// num = 5
+// num = 0
+// if (num > 0) {
+//     console.log('number is positive')
+// } else if (num < 0) {
+//     console.log('number is negative')
+// } else {
+//     console.log('number is 0')
+// }
+
+// let color = 'red'
+// color = 'green'
+// color = 10
+// switch (color) {
+//     case 'red':
+//         console.log('color is red')
+//         break
+//     case 'blue':
+//         console.log('color is blue')
+//         break
+//     case 'green':
+//         console.log('color is green')
+//         break
+//     default:
+//         console.log("not a valid color")
+// }
+
+// LOOPING
+// for (let i = 1; i <= 5; i++) {
+//     console.log("iteration #" + i)
+// }
+
+// let i = 1;
+// while(i <= 5) {
+//     console.log("iteration #" + i)
+//     i++
+// }
+
+// let i = 1
+// do {
+//     console.log("iteration #" + i)
+//     i++
+// } while (i <= 5)
+
+// const numArray = [1, 2, 3, 4, 5]
+// for (const num of numArray) {
+//     console.log("iteration #" + num)
+// }
+
+// FUNCTIONS
+// function greet(username) {
+//     console.log("Good morning, " + username + "!")
+// }
+// greet('Zach')
+// greet('Cody')
+// greet('Mosby')
+
+// let sum
+// function add(a, b) {
+//     return a + b
+// }
+// sum = add(5, 10)
+// console.log(sum)
+// sum = add(35, 33)
+// console.log(sum)
+
+// const arrowSum = (a, b) => a + b
+// sum = arrowSum(5, 10)
+// console.log(sum)
+// sum = arrowSum(35, 33)
+// console.log(sum)
+
+// const addFive = num => num + 5
